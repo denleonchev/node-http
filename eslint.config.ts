6a675ee953000caa19b18d5fcd60cd70efcaf12a
den.leonchev@gmal.com <den.leonchev@gmail.com>
2026-02-08 19:46:19 +0000

    fix eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,5 +7,17 @@ import eslintConfigPrettier from 'eslint-config-prettier/flat';
 export default defineConfig([
   { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
-  eslintConfigPrettier
+  eslintConfigPrettier,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ]
+    }
+  },
 ]);
